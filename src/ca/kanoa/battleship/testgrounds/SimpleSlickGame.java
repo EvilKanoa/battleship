beb488da53cf023185645b292486028d61c6031f
EvilKanoa <kanoa@kanoa.ca>
2016-06-09 11:15:29 -0400

    updated testgrounds

--- src/ca/kanoa/battleship/testgrounds/SimpleSlickGame.java
+++ src/ca/kanoa/battleship/testgrounds/SimpleSlickGame.java
@@ -1,7 +1,39 @@
 package ca.kanoa.battleship.testgrounds;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
-/**
- * Created by kanoa on 2016-06-07.
- */
-public class SimpleSlickGame {
+public class SimpleSlickGame extends BasicGame
+{
+    public SimpleSlickGame(String gamename)
+    {
+        super(gamename);
+    }
+
+    @Override
+    public void init(GameContainer gc) throws SlickException {}
+
+    @Override
+    public void update(GameContainer gc, int i) throws SlickException {}
+
+    @Override
+    public void render(GameContainer gc, Graphics g) throws SlickException {
+        g.drawString("Howdy!", 40, 60);
+    }
+
+    public static void main(String[] args) {
+        try {
+            AppGameContainer appgc;
+            appgc = new AppGameContainer(new SimpleSlickGame("Simple Slick Game"));
+            appgc.setDisplayMode(640, 480, false);
+            appgc.start();
+        }
+        catch (SlickException ex) {
+            Logger.getLogger(SimpleSlickGame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
